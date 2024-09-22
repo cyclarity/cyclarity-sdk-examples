81b0b39d1b0d3f8cfe4f98378f7bc23d93de6006
tomer shoob <tomer.shoob@cymotive.com>
2024-09-22 15:13:32 +0300

    update runnable usage example

--- my-new-runnable/my_new_runnable/main.py
+++ my-new-runnable/my_new_runnable/main.py
@@ -1,28 +1,35 @@
 from cyclarity_sdk.expert_builder import Runnable, BaseResultsModel
-from cyclarity_sdk.sdk_models import ExecutionStatus
 import time
+from cyclarity_sdk.sdk_models.findings import PTFinding
+import cyclarity_sdk.sdk_models.findings.types as PTFindingTypes
 
 
 class MyRunResult(BaseResultsModel):
     res: str = ""
 
 
-class MyRun(Runnable[MyRunResult]):
+class MyRunnable(Runnable[MyRunResult]):
     input_int: int = 100
 
     def setup(self):
-        self.logger.info("SETUP")
-        self.platform_api.send_test_report_description("My runnable description!")
+        self.logger.info("SETUP before running")
+        self.platform_api.send_test_report_description("My test description")
 
     def run(self, *args, **kwargs):
+        self.logger.info("RUNNING")
         res = 0
-        for i in range(self.input_int):
-            self.logger.info(i)
-            self.platform_api.send_execution_state(i, ExecutionStatus.RUNNING, "")
-            time.sleep(0.5)
-            res += i
+        for percentage in range(101):
+            self.platform_api.report_test_progress(percentage=percentage)
+            time.sleep(0.01)
         return MyRunResult(res=f"{res}")
 
     def teardown(self, exception_type=None, exception_value=None, traceback=None):
-        self.logger.info("TEARDOWN")
-
+        self.logger.info("TEARDOWN after run function")
+        my_finding=PTFinding(topic='My finding',
+                             status=PTFindingTypes.FindingStatus.FINISHED,
+                             type=PTFindingTypes.FindingType.FINDING,
+                             assessment_category=PTFindingTypes.AssessmentCategory.FUNCTIONAL_TEST,
+                             assessment_technique=PTFindingTypes.AssessmentTechnique.OTHER_EXPLORATION,
+                             purpose='Runnable example',
+                             description='This is a PTFinding Description runnable example')
+        self.platform_api.send_finding(my_finding)
